69762f6807ac870c78e12681a9721e9252b0a935
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-12 19:37:20 +0530

    basic code of java new version

--- src/p1basics/basicscode/basicscode.java
+++ src/p1basics/basicscode/basicscode.java
@@ -1,8 +1,15 @@
 package p1basics.basicscode;
 
 public class basicscode {
-    public static void main(String[] args) {
 
+    // OLD VERSION OF CODE
+
+   // public static void main(String[] args) {
+
+
+    // NEW VERSION OF CODE
+
+    void main() {
         System.out.println("Hello World");  // This is a Hello world
         System.out.println("Number: " + 3 );  // number
         System.out.println("String: " + "4 + 3");  // String
