453d116e7ff2de0029e9d0c460ca11520a2f2271
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-08-12 19:31:49 +0530

    basic code of java

---
+++ src/p1basics/basicscode/basicscode.java
@@ -0,0 +1,12 @@
+package p1basics.basicscode;
+
+public class basicscode {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");  // This is a Hello world
+        System.out.println("Number: " + 3 );  // number
+        System.out.println("String: " + "4 + 3");  // String
+        System.out.println("Expression: " + (4 + 3));    // Expression
+    }
+
+}
